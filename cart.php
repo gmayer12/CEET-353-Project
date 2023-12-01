c5f0f8ba807043c091656c0b87dba2bdbd419e6d
Gavin Gallagher <19ggallagher@gmail.com>
2023-12-01 16:50:01 -0500

    Updated cart functionality

--- cart.php
+++ cart.php
@@ -11,10 +11,21 @@ require 'header.html'; // Include the header
 <div id="content">
     <div class="cart">
     <h1>Cart</h1>
-    <p>Show current cart. Have purchase options</p>
+    <p>Your current cart:</p>
 
-    <p>Please tell us what you think.</p>
+    <div class="thumbnailContainer">
+        <?php 
 
+        // Loop through the array and display images
+        $imageArray = ['images/album_Jackson 4-22-23/1.JPG', 'images/album_Jackson 6-24-23/2.JPG'];
+        foreach ($imageArray as $imageName) {
+            echo '<div class="image-item">';
+            echo '<img class="albumCover" src="' . $imageName . '" width="25%" alt="Image"></span>';
+            echo '</div>';
+        }
+        ?>
+</div>
+    <div class="cart">
     <form action="processorder.php" method="post">
     <p><strong>Your name:</strong><br/>
     <input type="text" name="name" size="40" /></p>
@@ -26,8 +37,9 @@ require 'header.html'; // Include the header
     <textarea name="feedback" rows="8" cols="40">
     </textarea></p>
      
-    <p><input type="submit" value="Send Feedback" /></p>  
+    <p><input type="submit" value="Send Order" /></p>  
     </form>
+</div>
 </div>
 </body>
 </html>
